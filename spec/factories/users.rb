a8ea3c10aa3e4e16434d433039bbcaac69847b4e
Odell Purifoy <opurifoy@springhealth.com>
2023-05-13 14:41:56 -0400

    Seeded the DB

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,14 @@
+require 'faker'
+
 FactoryBot.define do
   factory :user do
-    
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email do 
+      Faker::Internet.email(
+        name: "#{first_name} #{last_name}",
+        separators: %w[+ _ .].sample 
+      )
+    end
   end
 end
